819ae8e4a24119abd5f91c18c379414ebb322648
Rico Callirgos <rico.callirgos@praxent.com>
2021-11-12 14:56:31 -0600

    Adds PUT and DELETE
    Signed-off-by: Rico Callirgos <rico.callirgos@praxent.com>

--- src/recipes/recipesController.ts
+++ src/recipes/recipesController.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Path, Post, Query, Route, SuccessResponse } from 'tsoa'
+import { Body, Controller, Get, Path, Post, Put, Delete, Query, Route, SuccessResponse } from 'tsoa'
 import { Recipe } from './recipe'
-import { RecipesService, RecipeCreationParams } from './recipesService'
+import { RecipesService, RecipeCreationParams, RecipeUpdateParams } from './recipesService'
 
 @Route('Recipes')
 export class RecipesController extends Controller {
@@ -19,5 +19,23 @@ export class RecipesController extends Controller {
   public async createRecipe(@Body() requestBody: RecipeCreationParams): Promise<Recipe> {
     this.setStatus(201)
     return new RecipesService().create(requestBody)
+  }
+
+  @SuccessResponse('201', 'Updated')
+  @Put('{RecipeId}')
+  public async updateRecipe(
+    @Path() RecipeId: number,
+    @Body() requestBody: RecipeUpdateParams,
+  ): Promise<Recipe> {
+    console.log(requestBody, 'RecipeUpdateParams')
+    this.setStatus(201)
+    return new RecipesService().update(RecipeId, requestBody)
+  }
+
+  @SuccessResponse('201', 'Deleted')
+  @Delete('{RecipeId}')
+  public async deleteRecipe(@Path() RecipeId: number): Promise<boolean> {
+    this.setStatus(201)
+    return new RecipesService().delete(RecipeId)
   }
 }
--- src/recipes/recipesService.ts
+++ src/recipes/recipesService.ts
@@ -4,6 +4,7 @@ const db = new JSONdb('./data/recipes.json')
 
 // A post request should not contain an id.
 export type RecipeCreationParams = Pick<Recipe, 'temp' | 'title' | 'time' | 'ingredients'>
+export type RecipeUpdateParams = Partial<Recipe>
 
 export class RecipesService {
   public get(id: number | undefined): Recipe {
@@ -33,5 +34,24 @@ export class RecipesService {
     db.set(id, recipe)
 
     return recipe
+  }
+
+  public update(RecipeId: number, RecipeUpdateParams: RecipeUpdateParams): Recipe {
+    const recipe = db.get(RecipeId)
+    if (!recipe) throw new Error('Recipe ID does not exist')
+
+    const updatedRecipe = Object.assign(recipe, RecipeUpdateParams)
+    db.set(RecipeId, updatedRecipe)
+
+    return updatedRecipe
+  }
+
+  public delete(id: number): boolean {
+    const recipe = db.get(id)
+    if (!recipe) throw new Error('Recipe ID does not exist')
+
+    const successfulDeletion: boolean = db.delete(id)
+
+    return successfulDeletion
   }
 }
